312fb4a1e861df1a26118cb61e1a2eacaeccaab2
agerho000 <andrew_gerhold@cable.comcast.com>
2014-11-20 18:27:17 -0800

    fully tested and working

--- main.go
+++ main.go
@@ -30,11 +30,10 @@ import "fmt"
 import "flag"
 import "io/ioutil"
 import "net/http"
-
-//import "reflect"
+import "bytes"
 import "encoding/xml"
 import "os"
-import "bytes"
+import "strings"
 
 type (
 	Config struct {
@@ -66,6 +65,28 @@ type (
 		Node    string `json:"Node"`
 		Address string `json:"Address"`
 	}
+
+	XMLload struct {
+		XMLName       xml.Name          `xml:"load"`
+		Duration      int               `xml:"duration,attr"`
+		Unit          string            `xml:"unit,attr"`
+		ArrivalPhases []XMLarrivalphase `xml:"arrivalphase"`
+	}
+
+	XMLarrivalphase struct {
+		XMLName  xml.Name `xml:"arrivalphase"`
+		Phase    int      `xml:"phase,attr"`
+		Duration int      `xml:"duration,attr"`
+		Unit     string   `xml:"unit,attr"`
+		Users    XMLusers `xml:"users"`
+	}
+
+	XMLusers struct {
+		XMLName     xml.Name `xml:"users"`
+		Maxnumber   int      `xml:"maxnumber,attr"`
+		Arrivalrate int      `xml:"arrivalrate,attr"`
+		Unit        string   `xml:"unit,attr"`
+	}
 )
 
 var (
@@ -81,7 +102,7 @@ func check(e error) {
 
 func NewXMLclient(host string) *XMLclient {
 	return &XMLclient{Host: host, Weight: 1, CPU: 4, Maxusers: 60000, Ip: []XMLip{
-		XMLip{Scan: "yes", Value: "eth0"}}}
+		XMLip{Scan: "true", Value: "eth0"}}}
 } //NewXMLclient
 
 func main() {
@@ -126,5 +147,79 @@ func getAvailableNodesFromCatalog() {
 		servers.Clients = append(servers.Clients, *NewXMLclient(node.Node))
 	}
 
-	fmt.Printf("%+v\n", servers)
+	fmt.Printf("%+v\n\n", servers)
+
+	load := XMLload{Duration: 1, Unit: "minute",
+		ArrivalPhases: []XMLarrivalphase{
+			XMLarrivalphase{
+				Phase:    1,
+				Duration: 1,
+				Unit:     "minute",
+				Users: XMLusers{
+					Maxnumber:   60000,
+					Arrivalrate: 100,
+					Unit:        "minute"}}}}
+
+	fd, err := os.Open("/home/parallels/Desktop/gocode/src/tsunginator/examples/boris_go_loadtest.xml")
+	check(err)
+
+	bufferForFile := new(bytes.Buffer)
+	bufferForFile.ReadFrom(fd)
+	fileAsString := bufferForFile.String()
+
+	modifyXmlValues(fileAsString, load, servers)
+}
+
+func testOutput() {
+	cloud := Clients{}
+	cloud = Clients{
+		Clients: []XMLclient{
+			XMLclient{Host: "cmbload01", Weight: 1, CPU: 4, Maxusers: 40000},
+			XMLclient{Host: "cmbload02", Weight: 1, CPU: 4, Maxusers: 40000},
+			XMLclient{Host: "cmbload03", Weight: 1, CPU: 4, Maxusers: 40000}}}
+	fmt.Printf("%+v\n", cloud)
+}
+
+func modifyXmlValues(content string, load XMLload, cloud Clients) {
+	var buffer bytes.Buffer
+	inputReader := strings.NewReader(content)
+	decoder := xml.NewDecoder(inputReader)
+	encoder := xml.NewEncoder(&buffer)
+	encoder.Indent("", " ")
+	buffer.WriteString(xml.Header)
+	buffer.WriteString("<!DOCTYPE tsung SYSTEM '/usr/share/tsung/tsung-1.0.dtd'>\n")
+	for {
+		t, _ := decoder.Token()
+		if t == nil {
+			break
+		}
+		switch token := t.(type) {
+		case xml.StartElement:
+			fmt.Println(t)
+			switch token.Name.Local {
+			case "load":
+				encoder.Encode(load)
+			case "clients":
+				encoder.Encode(cloud)
+			case "client", "arrivalphase", "users":
+				// allows me to ignore the inner element of this
+				// I probably should explain why this is necessary
+				// I can't remember myself as of right now
+			default:
+				err := encoder.EncodeToken(t)
+				check(err)
+			}
+		case xml.EndElement:
+			switch token.Name.Local {
+			case "clients", "load", "client", "arrivalphase", "users":
+				// allows me to ignore end tag errors like
+				// xml: end tag </client> does not match start tag <tsung>
+			default:
+				err := encoder.EncodeToken(t)
+				check(err)
+			}
+		}
+	}
+	encoder.Flush()
+	fmt.Println(buffer.String())
 }
